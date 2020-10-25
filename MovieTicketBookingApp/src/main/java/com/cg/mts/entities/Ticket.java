4bb17f7eaefee2381ce76fdde1b0ec6b2f3e552e
Rkstar <Rkstar@DESKTOP-9NH6C61>
2020-10-25 21:51:11 +0530

    ticket entity modify

--- MovieTicketBookingApp/src/main/java/com/cg/mts/entities/Ticket.java
+++ MovieTicketBookingApp/src/main/java/com/cg/mts/entities/Ticket.java
@@ -10,5 +10,57 @@ private Booking bookingRef;
 private boolean ticketStatus;
 private String screenName;
 
+public Ticket()
+{
+	super();
+}
+
+public int getTicketId() {
+	return ticketId;
+}
+
+public void setTicketId(int ticketId) {
+	this.ticketId = ticketId;
+}
+
+public int getNoOfSeats() {
+	return noOfSeats;
+}
+
+public void setNoOfSeats(int noOfSeats) {
+	this.noOfSeats = noOfSeats;
+}
+
+public List<String> getSeatNumber() {
+	return seatNumber;
+}
+
+public void setSeatNumber(List<String> seatNumber) {
+	this.seatNumber = seatNumber;
+}
+
+public Booking getBookingRef() {
+	return bookingRef;
+}
+
+public void setBookingRef(Booking bookingRef) {
+	this.bookingRef = bookingRef;
+}
+
+public boolean isTicketStatus() {
+	return ticketStatus;
+}
+
+public void setTicketStatus(boolean ticketStatus) {
+	this.ticketStatus = ticketStatus;
+}
+
+public String getScreenName() {
+	return screenName;
+}
+
+public void setScreenName(String screenName) {
+	this.screenName = screenName;
+}
 
 }
